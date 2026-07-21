78a95b89656c237997e964045a1feb1634d97409
Helmy-434 <youssefhelmy1113@gmail.com>
2026-07-21 21:19:17 +0300

    Full Implementation
    Deployment next

---
+++ Backend/models/Url.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    originalUrl: { type: String, required: true },
+    shortUrl: { type: String, required: true, unique: true },
+    clicks: { type: Number, default: 0 }
+},{ timestamps: true });
+
+module.exports = mongoose.model('Url', urlSchema);
