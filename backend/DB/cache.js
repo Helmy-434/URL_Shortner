b6ab74ff371685768c87f6abed49995c56939680
Helmy-434 <youssefhelmy1113@gmail.com>
2026-08-22 02:05:13 +0300

    Updated Docker for DB hosting

--- backend/DB/cache.js
+++ backend/DB/cache.js
@@ -1,7 +1,7 @@
 const Redis = require('redis');
 
 const redisClient = Redis.createClient({
-    url: process.env.REDIS_URL 
+    url: process.env.REDIS_URL || 'redis://redis:6379 '
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
