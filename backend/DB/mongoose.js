f322935e68f5fdca9fe42ee45ca45f507d10eaa2
Helmy-434 <youssefhelmy1113@gmail.com>
2026-08-22 04:06:11 +0300

    Final Update

--- backend/DB/mongoose.js
+++ backend/DB/mongoose.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     console.log("Checking MONGODB_URI:");
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        const MONGO_URI = process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://mongo:27017/urlshortener';
+        await mongoose.connect(MONGO_URI);
         console.log(' MongoDB connected successfully!');
     } catch (err) {
         console.error(' Connection failed!', err);
